f0cf721e1982d30fa58030f2f01523c9e8c9ba12
osawa-koki <github@osawa.dev>
2022-12-31 16:41:30 +0900

    小さな修正。

--- app/app.go
+++ app/app.go
@@ -13,6 +13,9 @@ func Start() {
 	// ハンドラファンクション -> デフォルトmultiplexerに登録。 | 自身で作成したmultiplexerを使用
 	// 第一引数 -> パターン
 	// 第二引数 -> 「レスポンスライター」「リクエスト」を引数として受け取る関数
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Index"))
+	})
 	router.HandleFunc("/greet", greet)
 	router.HandleFunc("/customers", getAllCustomers)
 	router.HandleFunc("/customers_xml", getAllCustomers_xml)
